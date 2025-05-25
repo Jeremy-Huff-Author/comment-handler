900876f815ec01775f24711fd49827376a247687
jeremythuff <jeremythuff@gmail.com>
2025-05-25 06:34:40 +0000

    changed prop name

--- netlify/functions/mailing-list-handler.js
+++ netlify/functions/mailing-list-handler.js
@@ -32,7 +32,7 @@ exports.handler = async (event) => {
         database_id: databaseId,
       },
       properties: {
-        Name: {
+ 'Contact Name': {
           title: [
             {
               text: {
@@ -45,9 +45,7 @@ exports.handler = async (event) => {
           email: email,
         },
         'Status': { // Add the Status property
-          select: {
-            name: 'Active', // Set the default status to Active
-          },
+ 'select': { name: 'Active' }, // Set the default status to Active
         },
       },
       // Ensure other properties from your Notion database are included here if required and have default values set in Notion
